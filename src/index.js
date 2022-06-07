70fea2daec693704b24b56b02f68ee32d1291890
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2022-06-08 00:03:48 +0300

    Реализован серверный скрипт, возвращающий статичные html страницы

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('/dist/index.html'));
+app.use(express.static('./dist'));
+
+app.listen(PORT);
