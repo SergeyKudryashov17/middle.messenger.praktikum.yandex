034c9bbfacff69cb727d375241148098dc46c5dd
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2022-12-09 01:04:17 +0300

    Мелкие правки по оформлению кода

--- src/pages/404/index.ts
+++ src/pages/404/index.ts
@@ -1,18 +1,18 @@
 import Link from "../../components/link/Link";
-import renderDOM from '../../core/renderDOM';
+import renderDOM from "../../core/renderDOM";
 import ErrorPage from "../../components/errorPage/ErrorPage";
 
 const linkBackChats: Link = new Link({
-    href: '#',
-    className: '',
-    label: 'Назад к чатам'
+    href: "#",
+    className: "",
+    label: "Назад к чатам",
 });
 
 const pageNotFound: ErrorPage = new ErrorPage({
-    title: '404',
-    typeBody: 'default',
-    caption: 'Не туда попали',
-    link: linkBackChats
+    title: "404",
+    typeBody: "default",
+    caption: "Не туда попали",
+    link: linkBackChats,
 });
 
 renderDOM("#app", pageNotFound);
