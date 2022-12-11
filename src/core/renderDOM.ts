c1929c3ec2f5ce96a0f8e4ac3cf4e982fd93a239
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2022-12-12 00:56:02 +0300

    Дорааботана обработка типов

--- src/core/renderDOM.ts
+++ src/core/renderDOM.ts
@@ -1,7 +1,9 @@
 import Block from "./Block";
 
-export default function renderDOM(selector: string, block: Block): HTMLElement {
-    const root: HTMLElement = document.querySelector(selector);
+export default function renderDOM(selector: string, block: Block): HTMLElement | null {
+    const root: HTMLElement | null = document.querySelector(selector);
+
+    if (!root) return null;
 
     root.innerHTML = '';
     root.appendChild(block.getContent());
