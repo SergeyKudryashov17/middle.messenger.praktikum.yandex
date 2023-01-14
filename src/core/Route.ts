ef36e50b720533484fb8f1e3abfb8ea7e6007c94
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2023-01-14 14:22:01 +0300

    Реализованы классы Router и Route

---
+++ src/core/Route.ts
@@ -0,0 +1,43 @@
+import renderDOM from "./renderDOM";
+import Block from "./Block";
+
+export default class Route {
+  private pathname: string;
+  private blockClass: string;
+  private block: Block | null;
+  private props: Record<string, any>;
+
+  constructor(pathname: string, view: string, props: Record<string, any>) {
+    this.pathname = pathname;
+    this.blockClass = view;
+    this.block = null;
+    this.props = props;
+  }
+
+  navigate(pathname: string) {
+    if (this.match(pathname)) {
+      this.pathname = pathname;
+      this.render();
+    }
+  }
+
+
+  leave() {
+    if (this.block) {
+      this.block.hide();
+    }
+  }
+
+  match(pathname: string) {
+    return pathname === this.pathname;
+  }
+
+  render() {
+    if (!this.block) {
+      this.block = this.blockClass;
+      renderDOM(this.props.rootQuery, this.block as Block);
+    }
+
+    this.block.show();
+  }
+}
---
+++ src/core/Router.ts
@@ -0,0 +1,55 @@
+import Block from "./Block";
+import Route from "./Route";
+
+export default class Router {
+  public routes:Route[] = [];
+  public history:History = window.history;
+  private currentRoute: null = null;
+  private static instance: Router;
+  private rootQuery: string;
+
+  constructor(rootQuery: string) {
+    if (Router.instance) {
+      return Router.instance;
+    }
+    this.rootQuery = rootQuery;
+    Router.instance = this;
+  }
+
+  use(pathname: string, block: Block): Router {
+    const route: Route = new Route(pathname, block, {rootQuery: this.rootQuery});
+    this.routes.push(route);
+    return this;
+  }
+
+  start() {
+    window.onpopstate = (event: Event) => {
+      this.onRoute(event.currentTarget.location.pathname);
+    }
+    console.log(window.location.pathname);
+    this.onRoute(window.location.pathname);
+  }
+
+  private onRoute(pathname: string) {
+    const route = this.getRoute(pathname);
+
+    console.log(route);
+
+    if (!route) return;
+
+    if (this.currentRoute) {
+      this.currentRoute.leave();
+    }
+
+    route.render();
+  }
+
+  go(pathname: string) {
+    this.history.pushState({}, "", pathname);
+    this.onRoute(pathname);
+  }
+
+  getRoute(pathname: string) {
+    return this.routes.find(route => route.match(pathname));
+  }
+}
